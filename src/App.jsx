c8368e536b800b1b2b51bdf50c160e0ea8ba775d
Hamza-Khan-official <kh.hamza56789@gmail.com>
2026-01-04 00:17:05 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -2,39 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-// import Input from './Components/Input/Input_.jsx'
-import Button from './Components/Buttons/Button.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  // const [count, setCount] = useState(0)
-  let input;
+  const [count, setCount] = useState(0)
 
-  
-  // function click_Button() {
-    //   console.log("Hyyy")
-    // }
-    var click_Button = () => {
-      console.log(input.value)
-    }
   return (
     <>
-
-      <div style={{ display: 'flex', gap: 20 }}>
-        {/* <input type="text" name="" id="" /> */}
-
-        {
-          input = <input onClick={click_Button()} type="text" name="" id="" />
-        }
-        {/* <Input /> */}
-        {/* <Button butt_Name="Add" /> */}
-        <button onClick={click_Button()} type="submit">Add</button>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-
-
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
-
 }
 
 export default App
