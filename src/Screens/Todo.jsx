2b1e02099b2f0b7169c434716f223603581d90ea
Hamza-Khan-official <kh.hamza56789@gmail.com>
2026-01-14 00:20:05 +0500

    add

--- src/Screens/Todo.jsx
+++ src/Screens/Todo.jsx
@@ -1,25 +1,62 @@
 import React, { useState } from 'react'
 import styles from './Todo.module.css'
+import { use } from 'react'
 
 const Todo = () => {
 
-    const [input, setInput] = useState("")
+    // Add Input / Add_TODO
+    const [input, setInput] = useState({
+        addtodo:"",
+    })
+
+    // Merge TODO IN ARRAY
+    const [InputArray, setInputArray] = useState([])
+
+    // DELL SINGLE TODO
+    // const [delSignleTodo, setDelSignleTodo] = useState("")
 
     let todo = () => {
-        console.log(input)
+        setInputArray([...InputArray, input])
+        setInput({ addtodo: "" }); 
+        // console.log(InputArray);
+      
+    }
+
+    let delAll = () => {
+        setInputArray([])
+    }
+
+    let del_todo = (i) => {
+        var newArray = [...InputArray]
+        newArray.splice(i, 1)
+        setInputArray(newArray)
+
     }
 
     return (
         <div>
             <div className={styles.container} style={{ textAlign: "center" }}>
                 <div className={styles.inputwrapper}>
-                    <input onChange={(e) => setInput(e.target.value)} placeholder="Enter Todo..." id="todoInput" type="text" />
+                    <input onChange={(e) => setInput({addtodo: e.target.value})} placeholder="Enter Todo..." id="todoInput" type="text" />
                     <button onClick={todo} className={styles.editbtn} >Add Todo</button>
-                    <button className={styles.deletebtn}>Delete All</button>
+                    <button onClick={delAll} className={styles.deletebtn}>Delete All</button>
                 </div>
                 <hr />
                 <br /><br />
                 <ul id="list">
+
+                    {
+                        InputArray.map((e, i)=>{
+                            return  <li key={i}>{e.addtodo}
+                                    <button 
+                                     onClick={()=>{
+                                        // setDelSignleTodo(i),
+                                        del_todo(i);
+                                        }} className='btn btn-warning'>Delete</button>
+                                    <button className='btn btn-danger'>edit</button>
+                            </li>
+                        })
+                    }
                 </ul>
             </div>
         </div>
